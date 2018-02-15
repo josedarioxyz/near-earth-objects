0392cab57ccf1de8aabe189419df7e78a2813761
Jose Dario Sanchez <josedario.xyz@gmail.com>
2018-02-15 12:41:32 -0800

    Adds NASA API link + react-router-dom dependency

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,11 @@ const Header = ({ date }) => {
   return (
     <div id='header' className="jumbotron jumbotron-fluid">
       <div className="container">
-        <h6 className='text-right'>api.nasa.gov</h6>
+        <h6 className='text-right'>
+          <a className='header-api-link' href='https://api.nasa.gov/'>
+            api.nasa.gov
+          </a>
+        </h6>
         <Divider />
         <h4 className='header-date text-uppercase'>{date}</h4>
         <h1 className="text-uppercase">Near Earth Objects</h1>
